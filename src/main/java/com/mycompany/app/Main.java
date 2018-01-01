8695eac9e8711e945ecc5712c81980e197119256
RyoHashi <kuttc777@gmail.com>
2018-01-01 16:14:22 +0900

    First commit Hello new year !

---
+++ src/main/java/com/mycompany/app/Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/**
+ * IDEでrunする場合このクラスをmainクラスに設定する
+ */
+
+public class Main {
+    static public void main(String arg[]) {
+        System.out.println("Hello new year !!!");
+    }
+}
